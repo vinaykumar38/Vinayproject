fb65d3eaeea4ffc84b8094ff0aafcf011e39bbf4
vinaykumar38 <subojivinaykumar3866@gmail.com>
2023-08-08 12:14:11 +0100

    files added

---
+++ YtBookStore/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace YtBookStore.Models.Domain
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base (options)
+        { 
+        }
+        //defineing Dbsets
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Publisher> Publisher{ get; set; }
+
+        public DbSet<Book> Book{ get; set; }
+
+    }
+}
---
+++ YtBookStore/Repositories/Abstract/IGenreService.cs
@@ -0,0 +1,17 @@
+﻿
+﻿using YtBookStore.Models.Domain;
+
+namespace YtBookStore.Repositories.Abstract
+
+    //in abstract all these are interfaces and in implementation all are classes
+{
+    public interface IGenreService
+    {
+        bool Add(Genre model);
+        bool Update(Genre model);
+        bool Delete(int id);
+        Genre FindById(int id);
+        IEnumerable<Genre> GetAll();
+    }
+}
+
